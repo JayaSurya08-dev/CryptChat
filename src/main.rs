2ab011b76edee5cc61f50dcc22f7d7c0e2400ee8
Jayasurya08-dev <suryabigboss08@gmail.com>
2026-08-13 01:41:09 +0530

    feat: Implemented a dummy client to send messages within the server.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,24 @@ async fn main(){
     let listener= TcpListener::bind("127.0.0.1:8000").await.unwrap();
     println!("Server is running on port 127.0.0.1:8000");
 
+    let writer =tokio::spawn({
+        async move{
+            let mut client=tokio::net::TcpStream::connect("127.0.0.1:8000").await.unwrap();
+            
+            let messages= tokio::io::BufReader::new(tokio::io::stdin());
+            let mut lines=messages.lines();
+
+            loop{
+                if let Some(line)=lines.next_line().await.unwrap(){
+                    client.write_all(line.as_bytes()).await.unwrap();
+                    client.write_all(b"\n").await.unwrap();
+                }else{
+                    break;
+                }
+            }
+
+        }
+    });
     let (socket,addr)=listener.accept().await.unwrap();
     println!("New connection from: {}",addr);
 
@@ -16,5 +34,10 @@ async fn main(){
     }
 
     println!("Connection closed");
+
+    tokio::select!{
+        _=writer=>{},
+        _=tokio::signal::ctrl_c()=>{},
+    }
 
 }
